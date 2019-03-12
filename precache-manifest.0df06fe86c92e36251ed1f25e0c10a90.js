58beac4c6f92ebfceb2385d7f57b84102ee4e166
Anatoliy Bayda <otikas77@gmail.com>
2019-03-12 15:33:21 +0200

    Updates

---
+++ precache-manifest.0df06fe86c92e36251ed1f25e0c10a90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e541f7ab8ff15830769",
+    "url": "/calculator/static/css/main.f8d1d6c5.chunk.css"
+  },
+  {
+    "revision": "9e541f7ab8ff15830769",
+    "url": "/calculator/static/js/main.bd0ee7da.chunk.js"
+  },
+  {
+    "revision": "0d721d06f78c96d55f57",
+    "url": "/calculator/static/js/runtime~main.19f1d22d.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/calculator/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "f7a5a0b50f0d2510b5804f1d9a95ed53",
+    "url": "/calculator/index.html"
+  }
+];
